6d7967161b89a0d3642a8346135940d6c78b2adb
Juandiegomok21 <juandiegomok@gmail.com>
2026-05-12 03:38:44 +0000

    Proyecto corregido en el archivo app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our', 'his'];
+let adj = ['great', 'big', 'amazing' ];
+let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
 
+// generating the domains
+let domains = [];
+for(let i = 0; i < pronoun.length; i++) {
+  for(let j = 0; j < adj.length; j++) {
+    for(let k = 0; k < noun.length; k++) {
+      let domain = pronoun[i] + adj[j] + noun[k]+'.com';
+      domains.push(domain);
+      console.log(domain); 
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.body.innerHTML += '<div class="container mt-5"><h2>Generated Domains:</h2><ul id="domains-list"></ul></div>';
+domains.forEach(domain => {
+  const li = document.createElement('li');
+  li.textContent = domain;
+  document.getElementById('domains-list').appendChild(li);
+});
